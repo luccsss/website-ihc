f527fbce05cfeeb0dd375ff41c0dc499504c1e07
Luciana Sanchez <lucianasanchez20152@gmail.com>
2024-07-04 13:00:20 -0500

    new

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form[name="frm"]');
+form.addEventListener('submit', (event) => {
+    const fname = form.elements['fname'].value;
+    const lname = form.elements['lname'].value;
+    const codi = form.elements['codi'].value;
+    const correo = form.elements['correo'].value;
+    const celular = form.elements['celular'].value;
+
+    if (!fname || !lname || !correo || !celular || !codi) {
+        event.preventDefault();
+        alert('Por favor, complete todos los campos');
+    } else if (!validateEmail(correo)) {
+        event.preventDefault();
+        alert('Por favor, ingrese un correo válido');
+    } else {
+        alert('GRACIAS POR REGISTRARSE');
+        console.log('Redirigiendo a a la pagina principal');
+        window.location.href = "AulaVirtual/hmtl/index1.html"; 
+    }
+});
+
+function validateEmail(correo) {
+    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return re.test(String(correo).toLowerCase());
+}
+function scrollToIndex(){
+    window.location.href = 'index.html';
+}
+function scrollToForm(){
+    var elemento = document.getElementById("formulario");
+    elemento.scrollIntoView({behavior: 'smooth'});
+}
